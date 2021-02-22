04050ae82f625a47ca82fff4fbbbbf8c1fcf94b0
cat kaka <1046376300@qq.com>
2021-02-22 17:24:22 +0800

    更新配置文件结构

--- src/main/model/Config.java
+++ src/main/model/Config.java
@@ -1,95 +1,90 @@
-package main.model;
+package main.model
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.*
+import java.util.regex.Pattern
 
-public class Config {
-
-    public static List<Integer> delayTimeList;
-    public static List<Stock> stockList;
-    public static String title;
-
-    public static void init(){
-        loadSettingConfig();
-        loadStockConfig();
+object Config {
+    @JvmField
+    var delayTimeList: MutableList<Int>? = null
+    @JvmField
+    var stockList: MutableList<Stock>? = null
+    @JvmField
+    var title: String? = null
+    @JvmStatic
+    fun init() {
+        loadSettingConfig()
+        loadStockConfig()
     }
 
-    private static void loadSettingConfig(){
-        if(delayTimeList == null){
-            delayTimeList = new ArrayList<>();
+    private fun loadSettingConfig() {
+        if (delayTimeList == null) {
+            delayTimeList = ArrayList()
         }
-        BufferedReader br = null;
-
+        var br: BufferedReader? = null
         try {
-            String filePath = "config.txt";
-            StringBuilder sb = new StringBuilder();
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
-            String temp;
-
-            while((temp = br.readLine()) != null){
-                sb.append(temp);
-                sb.append("\n");
+            val filePath = "config.txt"
+            val sb = StringBuilder()
+            br = BufferedReader(InputStreamReader(FileInputStream(filePath)))
+            var temp: String?
+            while (br.readLine().also { temp = it } != null) {
+                sb.append(temp)
+                sb.append("\n")
             }
-
-            temp = sb.toString().trim();
-            Matcher m = Pattern.compile("=\"\\S+\"").matcher(temp);
-            while(m.find()){
-                String s = m.group();
-                s = s.substring(2,s.length() - 1);
-                if(isInteger(s)){
-                    delayTimeList.add(Integer.parseInt(s));
-                }else{
-                    title = s;
+            temp = sb.toString().trim { it <= ' ' }
+            val m = Pattern.compile("=\"\\S+\"").matcher(temp)
+            while (m.find()) {
+                var s = m.group()
+                s = s.substring(2, s.length - 1)
+                if (isInteger(s)) {
+                    delayTimeList!!.add(s.toInt())
+                } else {
+                    title = s
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally {
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
             try {
-                assert br != null;
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                assert(br != null)
+                br!!.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
         }
     }
 
-    private static void loadStockConfig(){
-        if(stockList == null){
-            stockList = new ArrayList<>();
+    private fun loadStockConfig() {
+        if (stockList == null) {
+            stockList = ArrayList()
         }
-        String filePath = "stock.txt";
-        BufferedReader br = null;
-
+        val filePath = "stock.txt"
+        var br: BufferedReader? = null
         try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
-            String temp;
-
-            while ((temp = br.readLine()) != null){
-                String[] ss = temp.trim().split("\\s+");
-                Stock stock = new Stock(ss[0],ss[1],ss[2],ss[3]);
-                stockList.add(stock);
+            br = BufferedReader(InputStreamReader(FileInputStream(filePath)))
+            var temp: String
+            while (br.readLine().also { temp = it } != null) {
+                val ss = temp.trim { it <= ' ' }.split("\\s+".toRegex()).toTypedArray()
+                val stock = Stock(ss[0], ss[1], ss[2], ss[3])
+                stockList!!.add(stock)
             }
-        }catch(Exception e) {
-            e.printStackTrace();
-        }finally {
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
             try {
-                assert br != null;
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                assert(br != null)
+                br!!.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
         }
     }
 
-    private static boolean isInteger(String s){
-        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
-        return pattern.matcher(s).matches();
+    private fun isInteger(s: String): Boolean {
+        val pattern = Pattern.compile("^[-+]?[\\d]*$")
+        return pattern.matcher(s).matches()
     }
 }
--- src/main/model/Stock.java
+++ src/main/model/Stock.java
@@ -1,17 +1,11 @@
-package main.model;
+package main.model.bean;
 
-import com.sun.glass.events.KeyEvent;
-import main.Util.StockUtil;
-import main.Util.TimeUtil;
+import main.model.TradeMethod;
+import main.util.StockUtil;
+import main.util.TimeUtil;
+import okhttp3.Request;
 
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-
-import static main.Util.StockUtil.*;
+import static main.util.StockUtil.timeToInt;
 
 public class Stock {
     public String stockCode;   //  股票代码
@@ -56,45 +50,45 @@ public class Stock {
 
     public float changeRate;  //涨幅
 
-    public String dealMethod;  //交易方法
     public String startDate;  //开始日期
     public String dealNum;           //交易数量
     public int startMonth;         //开始月份
     public int startDay;        //开始日子
     public boolean isDeal;        //是否已经成交
 
-    private URL url;   //请求url
-    private final StringBuilder sb;   //留给下面update复用,避免大量构造重复对象
+    public boolean isAvailable;
+    public int index;
+    public TradeMethod tradeMethod;   //交易方式
+    public String dealMethod;    //交易方式,同上,只是字符形式的
+    public boolean isBuy;   //是买入还是卖出
+    private final Request request;
 
-    public Stock(String stockCode,String dealMethod,String dealNum,String startDate) {
+    public Stock(int index,String stockCode,String tradeMethod,String dealNum,String startDate) {
         this.stockCode = stockCode;
-        this.dealMethod = dealMethod;
+        this.dealMethod = tradeMethod;
+        this.tradeMethod = TradeMethod.getTradeMethod(tradeMethod);
         this.dealNum = dealNum;
         this.startDate = startDate;
         this.startMonth = Integer.parseInt(startDate.substring(0,startDate.indexOf('.')));
         this.startDay = Integer.parseInt(startDate.substring(startDate.indexOf('.') + 1));
-        this.sb = new StringBuilder();
-        try {
-            this.url = new URL("http://hq.sinajs.cn/list=" + (stockCode.charAt(0) == '6' ? "sh" : "sz") + stockCode);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        this.index = index;
+
+        char ch = tradeMethod.charAt(0);
+        this.isBuy = ch == 'b' || ch == 'B';
+
+        String url = StockUtil.BASE_URL + (stockCode.charAt(0) == '6' ? "sh" : "sz") + stockCode;
+        this.request = new Request.Builder().url(url).build();
         update();
     }
 
     //从接口获得数据，然后生成实体类
     public void update(){
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "gbk"));
-            String contentText;
-            String temp;
+            String[] vars = StockUtil.requestData(request).split(",");
 
-            while((temp = br.readLine()) != null){
-                sb.append(temp);
+            if (vars.length < 32){
+                return;
             }
-            contentText = sb.toString().trim();
-
-            String[] vars = contentText.split(",");
 
             //股票名称不用每次都赋值，赋值一次就够了
             if(stockName == null){
@@ -132,219 +126,19 @@ public class Stock {
             soldFivePrice = Float.parseFloat(vars[29]);
             date = vars[30];
             time = timeToInt(vars[31]);
-            changeRate = round((nowPrice / lastDayClosePrice - 1) * 100);
-            limitUpPrice = round((float) (lastDayClosePrice * 1.1));
+            changeRate = StockUtil.round((nowPrice / lastDayClosePrice - 1) * 100);
+            limitUpPrice = StockUtil.round((float) (lastDayClosePrice * 1.1));
 
-            sb.delete(0,sb.length());
+            isAvailable = true;
         } catch (Exception e) {
             e.printStackTrace();
+            isAvailable = false;
         }
     }
 
-    public boolean soldWay1() {
-        if(time < 34200)
-            return false;
 
-        if(todayHighestPrice < lastDayClosePrice) {
-            if(nowPrice < lastDayClosePrice * 0.95) {
-                if(time < 35100) {
-                    return nowPrice < lastDayClosePrice * 0.93;
-                } else{
-                    return true;
-                }
-            }
-        } else {
-            return nowPrice < todayHighestPrice * 0.95;
-        }
-        return false;
-    }
-
-    public boolean soldWay2() {
-        if(time < 33930)
-            return false;
-        return nowPrice >= limitUpPrice;
-    }
-
-    public boolean soldWay3() {
-        return false;
-    }
-
-    public boolean soldWay4() {
-        return false;
-    }
-
-    public boolean soldWay5() {
-        return false;
-    }
-
-    public boolean buyWay1() {
-        if(time <= 33900){
-            return false;     //  从9:25开始
-        }
-        return todayHighestPrice / lastDayClosePrice >= 1.09;
-    }
-
-    public boolean buyWay2() {
-        if(time <= 34200 && time >= 33930 ) {        //  从9:25:30 - 9:30:00
-            return changeRate >= 2;
-        }
-        return false;
-    }
-
-    public boolean buyWay3() {
-        return false;
-    }
-
-    public boolean buyWay4() {
-        return false;
-    }
-
-    public boolean buyWay5() {
-        return false;
-    }
-
-    public void buy() {
-        try {
-            Robot robot = StockUtil.getRobot();
-            List<Integer> list = Config.delayTimeList;
-
-            shiftProcess(Config.title);
-
-            robot.delay(list.get(1));
-
-            robot.keyPress(KeyEvent.VK_F2);          //切换到卖出界面 , 缓冲一下
-            robot.keyRelease(KeyEvent.VK_F2);
-            robot.delay(list.get(2));
-
-            robot.keyPress(KeyEvent.VK_F1);          //切换到买入界面
-            robot.keyRelease(KeyEvent.VK_F1);
-            robot.delay(list.get(3));
-
-            inputStock(stockCode,list.get(4));           // 输入股票代码
-
-            robot.delay(list.get(5));
-
-            robot.keyPress(KeyEvent.VK_TAB);    // 切换到买入价格
-            robot.keyRelease(KeyEvent.VK_TAB);
-            robot.delay(list.get(6));
-
-            robot.keyPress(KeyEvent.VK_BACKSPACE);        // 删掉预设价格
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.keyPress(KeyEvent.VK_BACKSPACE);
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.keyPress(KeyEvent.VK_BACKSPACE);
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.keyPress(KeyEvent.VK_BACKSPACE);
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.keyPress(KeyEvent.VK_BACKSPACE);
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.delay(list.get(7));
-
-            inputStock(String.valueOf(limitUpPrice),list.get(4));  // 自动输入涨停价
-            robot.delay(list.get(8));
-
-            robot.keyPress(KeyEvent.VK_TAB);    // 切换到买入数量
-            robot.keyRelease(KeyEvent.VK_TAB);
-            robot.delay(list.get(9));
-
-            inputStock(dealNum,list.get(4));            //  获取买入数量
-            robot.delay(list.get(10));
-
-            robot.keyPress(KeyEvent.VK_TAB);
-            robot.keyRelease(KeyEvent.VK_TAB);
-            robot.delay(list.get(11));
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
-            robot.delay(list.get(12));
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
-            robot.delay(list.get(13));
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
-            robot.delay(list.get(14));
-
-            shiftProcess("AutoTrade");
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public void sold(){
-        try {
-            Robot robot = new Robot();
-            List<Integer> list = Config.delayTimeList;
-            shiftProcess(Config.title);
-
-            robot.delay(list.get(0));
-
-            robot.keyPress(KeyEvent.VK_F1);          //切换到买入界面 , 缓冲一下
-            robot.keyRelease(KeyEvent.VK_F1);
-            robot.delay(list.get(1));
-
-            robot.keyPress(KeyEvent.VK_F2);          //切换到卖出界面
-            robot.keyRelease(KeyEvent.VK_F2);
-            robot.delay(list.get(2));
-
-            inputStock(stockCode,list.get(3));               // 输入股票代码
-            robot.delay(list.get(4));
-
-            robot.keyPress(KeyEvent.VK_TAB);    // 切换到卖出价格
-            robot.keyRelease(KeyEvent.VK_TAB);
-            robot.delay(list.get(5));
-
-            robot.keyPress(KeyEvent.VK_BACKSPACE);        // 删掉预设价格
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.keyPress(KeyEvent.VK_BACKSPACE);
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.keyPress(KeyEvent.VK_BACKSPACE);
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.keyPress(KeyEvent.VK_BACKSPACE);
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.keyPress(KeyEvent.VK_BACKSPACE);
-            robot.keyRelease(KeyEvent.VK_BACKSPACE);
-            robot.delay(list.get(6));
-
-            String price = Float.toString(round((float) (lastDayClosePrice * 0.9)));   // 自动输入跌停价
-            price = price.substring(0,price.indexOf('.') + 3);
-
-            inputStock(price,list.get(4));
-            robot.delay(list.get(7));
-
-            robot.keyPress(KeyEvent.VK_TAB);    // 切换到卖出数量
-            robot.keyRelease(KeyEvent.VK_TAB);
-            robot.delay(list.get(8));
-
-            inputStock(dealNum,list.get(4));            //  输入卖出数量
-            robot.delay(list.get(9));
-
-            robot.keyPress(KeyEvent.VK_TAB);
-            robot.keyRelease(KeyEvent.VK_TAB);
-            robot.delay(list.get(10));
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
-            robot.delay(list.get(11));
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
-            robot.delay(list.get(12));
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
-            robot.delay(list.get(13));
-            shiftProcess("AutoTrade");
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
 
     public boolean isShouldQuery(){
         return !isDeal && startMonth == TimeUtil.getMonth() && startDay == TimeUtil.getDay();
-    }
-
-    private static float round(float num) {
-        float temp = num * 100;
-        if(temp - Math.floor(temp) >= 0.5)
-            return (float) ((Math.floor(temp) + 1) / 100);
-        return (float) (Math.floor(temp) / 100);
     }
 }
